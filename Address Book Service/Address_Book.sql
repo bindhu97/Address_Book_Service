1f8e7d859d0319a188105a4d79898f72edbd609e
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-16 23:14:33 +0530

    [Bindhu]Add:Creating AddressBook Table

--- Address Book Service/Address_Book.sql
+++ Address Book Service/Address_Book.sql
@@ -2,3 +2,9 @@
 
 create Database Address_Book;
 use Address_Book;
+
+--UC2 Create Table Address_Book
+
+create Table Address_Book
+(FirstName varchar(100), LastName varchar(100), Address varchar(300), 
+City varchar(50), State varchar(30), ZipCode int, PhoneNumber bigint, EMailId varchar(50) )
